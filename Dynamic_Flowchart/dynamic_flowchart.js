184816b9d2a6a2a6a3d7b453c095947e48bb09c3
Kangbo Li <kli89@wisc.edu>
2016-12-03 11:24:05 -0600

    Add files via upload

---
+++ Dynamic_Flowchart/dynamic_flowchart.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Kangbo on 12/3/2016.
+ */
+function course(image, prereq, lecturer){
+    this.image = image;
+    this.prereq = prereq;
+    this.lecturer = lecturer;
+}
+function drawCourse(course, x, y){
+
+}
